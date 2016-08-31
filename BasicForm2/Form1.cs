22287eafe7fea4a1674f82aebcd80f00880488d8
Mayra Mendez <mmendez9@dtcc.edu>
2016-08-30 21:13:05 -0700

    Wage Calculator without message box

--- BasicForm2/Form1.cs
+++ BasicForm2/Form1.cs
@@ -16,5 +16,49 @@ namespace BasicForm2
         {
             InitializeComponent();
         }
+
+        private double textBox1_TextChanged()
+        {
+            string work = Console.ReadLine();
+            double hwork = Convert.ToDouble(work);
+            return hwork;
+        }
+
+        private double textBox2_TextChanged()
+        {
+            string pay = Console.ReadLine();
+            double hpay;
+            double.TryParse(pay, out hpay);
+            return hpay;
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Calculate the gross pay
+            //decimal hwork = textBox1.Text();
+            //decimal hpay = textBox2.Text();
+            //decimal cal = (Decimal.hwork * Decimal.hpay);
+            //MessageBox.Show("The Gross Pay is {0}", cal.ToString("c"));
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Clear the data
+            textBox1.Clear();
+            textBox2.Clear();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // Close the program
+            this.Close();
+        }
     }
 }
